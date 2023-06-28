5d74d73d17c0c1891efb4cf455806e7c6059d1cf
Fredrick Esposito <fredrickesposito@gmail.com>
2023-06-27 21:53:29 -0400

    Fetch dictionary data from lambda function

--- word_master/lib/main.dart
+++ word_master/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,24 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: _buildImportButton(),
         ),
       ),
+    );
+  }
+
+  Widget _buildImportButton() {
+    return ElevatedButton(
+      onPressed: () async {
+        String functionUrl =
+            "https://rgnbhyf5h63zg2krd6mxtr7cga0qlnse.lambda-url.us-east-1.on.aws/";
+        http.Response response = await http.get(Uri.parse(functionUrl));
+        print(response.body);
+      },
+      child: const Text('Import'),
     );
   }
 }

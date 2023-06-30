ff62dfe3d45689e3bf1615997959e3cd0b98bc99
Fredrick Esposito <fredrickesposito@gmail.com>
2023-06-29 22:43:14 -0400

    Remove comments

--- word_master/lib/word_table.dart
+++ word_master/lib/word_table.dart
@@ -33,19 +33,15 @@ class WordTable extends StatelessWidget {
       itemBuilder: (context, index) {
         List<TableCell> currentRowCells = [];
 
-        // i runs from (index*6) to ((index+1)*6 - 1) or the end of results
         for (int i = index * 6; i < min((index + 1) * 6, entries.length); i++) {
           currentRowCells
               .add(_buildTableCell(entries[i].wordOrPhrase, context));
         }
 
-        // TableRow needs at least one cell, so in a case when we have fewer
-        // than 6 words for the last row, we add empty cells
         while (currentRowCells.length < 6) {
           currentRowCells.add(_buildTableCell('', context));
         }
 
-        // Build a table for each row
         return Table(
           border: TableBorder.all(),
           children: [
